b50aa753f6dc6f4c4268549b26ef8157ee96c35b
gauravkumar2005 <gauravkumar8414pq@gmail.com>
2026-04-15 15:45:42 +0530

    change image

--- Frontend/src/pages/Jeans/Jeans.jsx
+++ Frontend/src/pages/Jeans/Jeans.jsx
@@ -1,6 +1,7 @@
 import Jeansimg from "../../assets/Jeansimg.jpg";
 import Jeansimg2 from "../../assets/Jeansimg2.jpg";
 import Jeansimg3 from "../../assets/Jeansimg3.jpg";
+import Girljeans1 from "../../assets/Girljeans1.jpg";
 
 function Jeans () {
   return(
@@ -62,7 +63,7 @@ function Jeans () {
 
         <div className="bg-white rounded-2xl shadow-md hover:shadow-xl hover:-translate-y-2 transition overflow-hidden">
           <img
-            src="https://images.unsplash.com/photo-1583005516764-9e0c2f8c1c62"
+            src={Girljeans1}
             className="w-full h-60 object-cover hover:scale-105 transition duration-300"
             alt="jeans"
           />

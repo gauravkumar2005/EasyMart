9b8f7dc117e48670ef95986b146e1d567e34b8ff
gauravkumar2005 <gauravkumar8414pq@gmail.com>
2026-04-15 15:48:24 +0530

    change image

--- Frontend/src/pages/Jeans/Jeans.jsx
+++ Frontend/src/pages/Jeans/Jeans.jsx
@@ -2,6 +2,7 @@ import Jeansimg from "../../assets/Jeansimg.jpg";
 import Jeansimg2 from "../../assets/Jeansimg2.jpg";
 import Jeansimg3 from "../../assets/Jeansimg3.jpg";
 import Girljeans1 from "../../assets/Girljeans1.jpg";
+import Stylishjeansgirl from "../../assets/Stylishjeansgirl.jpg";
 
 function Jeans () {
   return(
@@ -79,12 +80,12 @@ function Jeans () {
 
         <div className="bg-white rounded-2xl shadow-md hover:shadow-xl hover:-translate-y-2 transition overflow-hidden">
           <img
-            src="https://images.unsplash.com/photo-1612423284934-2850a4ea6b0f"
+            src={Stylishjeansgirl}
             className="w-full h-60 object-cover hover:scale-105 transition duration-300"
             alt="jeans"
           />
           <div className="p-4">
-            <h3 className="font-semibold text-lg">Black Jeans</h3>
+            <h3 className="font-semibold text-lg">Stylish Jeans</h3>
             <p className="text-gray-500 text-sm">Premium black style</p>
             <p className="text-indigo-600 font-bold text-xl mt-2">₹1399</p>
             <button className="mt-3 w-full bg-indigo-500 text-white py-2 rounded-lg hover:bg-indigo-600 transition">

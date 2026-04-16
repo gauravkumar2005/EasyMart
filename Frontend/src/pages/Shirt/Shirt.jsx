cf181ba5bdcf5320c7abb42112b78a84f4a37cdf
gauravkumar2005 <gauravkumar8414pq@gmail.com>
2026-04-16 14:54:19 +0530

    update image

--- Frontend/src/pages/Shirt/Shirt.jsx
+++ Frontend/src/pages/Shirt/Shirt.jsx
@@ -3,6 +3,8 @@ import Longshirt from "../../assets/Longshirt.jpg";
 import Fitgreenshirt from "../../assets/Fitgreenshirt.jpg";
 import Formalshirtgirl from "../../assets/Formalshirtgirl.jpg";
 import Longshirtgirl from "../../assets/Longshirtgirl.jpg";
+import Longshirtgirl from "../../assets/Longshirtgirl.jpg";
+import Styleshirtgirl from "../../assets/Stlyleshirtgirl.jpg";
 
 function Shirt () {
   return(
@@ -96,12 +98,12 @@ function Shirt () {
 
         <div className="bg-white rounded-2xl shadow-md hover:shadow-xl hover:-translate-y-2 transition overflow-hidden">
           <img
-            src="https://images.unsplash.com/photo-1621072156002-e2fccdc0b176"
+            src={Styleshirtgirl}
             className="w-full h-60 object-cover hover:scale-105 transition duration-300"
             alt="shirt"
           />
           <div className="p-4">
-            <h3 className="font-semibold text-lg">Linen Shirt</h3>
+            <h3 className="font-semibold text-lg">Stylish Shirt</h3>
             <p className="text-gray-500 text-sm">Light & breathable</p>
             <p className="text-blue-600 font-bold text-xl mt-2">₹1099</p>
             <button className="mt-3 w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition">

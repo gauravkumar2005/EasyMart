6fd4a3a7524e3b7322e79ff7431f10832c35e14d
Gaurav Kumar <gauravkumar8414pq@gmail.com>
2026-06-03 17:11:36 +0530

    addavailability time

--- Frontend/src/pages/Help/Help.jsx
+++ Frontend/src/pages/Help/Help.jsx
@@ -71,7 +71,8 @@ function Help() {
 
           <div className="space-y-2 text-gray-700">
             <p>Email: support@easymart.com</p>
-            <p>Phone: +91 9097710004</p>
+            <p>Phone: +91 90977 10004</p>
+            <p>Available: Monday - Saturday (9 AM - 7 PM)</p>
           </div>
         </div>
 

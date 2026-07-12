35af7f606d9bb4e29412f5880f87a001b95c8f7d
Gaurav Kumar <gauravkumar8414pq@gmail.com>
2026-07-12 23:27:43 +0530

    make add to card working for men.jsx

--- src/pages/Men/Men.jsx
+++ src/pages/Men/Men.jsx
@@ -67,7 +67,9 @@ function Men () {
             <p className="text-gray-500 text-sm">Soft & breathable</p>
             <p className="text-green-600 font-bold text-xl mt-2">₹399</p>
 
-            <button className="mt-3 w-full bg-green-500 text-white py-2 rounded-lg hover:bg-green-600 transition">
+            <button
+              onClick={() => addToCart({ id: 3, name: "Oversize T-Shirt", price: 399, img: Oversize })}
+              className="mt-3 w-full bg-green-500 text-white py-2 rounded-lg hover:bg-green-600 transition">
               Add to Cart
             </button>
           </div>
@@ -84,7 +86,9 @@ function Men () {
             <p className="text-gray-500 text-sm">Trendy design</p>
             <p className="text-green-600 font-bold text-xl mt-2">₹699</p>
 
-            <button className="mt-3 w-full bg-green-500 text-white py-2 rounded-lg hover:bg-green-600 transition">
+            <button 
+              onClick={() => addToCart({ id: 4, name: "Stylish Jeans", price: 699, img: Jeansimg })}
+              className="mt-3 w-full bg-green-500 text-white py-2 rounded-lg hover:bg-green-600 transition">
               Add to Cart
             </button>
           </div>

489202c92a4e6f55bc68d90993d6d4dbd659ff0c
Gaurav Kumar <gauravkumar8414pq@gmail.com>
2026-08-02 20:17:23 +0530

    add Buy Now buttons in women.jsx

--- Frontend/src/pages/Women/Women.jsx
+++ Frontend/src/pages/Women/Women.jsx
@@ -137,11 +137,18 @@ function Women () {
             <h3 className="font-semibold text-lg">Women Shirt</h3>
             <p className="text-gray-500 text-sm">Comfort fit</p>
             <p className="text-pink-600 font-bold text-xl mt-2">₹1199</p>
-            <button
+
+          <div className="mt-4 flex gap-3">
+             <button
               onClick={() => addToCart({ id: 5, name: "Women Shirt", price: 1199, img: Shirtgirl })}
               className="mt-3 w-full bg-pink-500 text-white py-2 rounded-lg hover:bg-pink-600 transition">
               Add to Cart
             </button>
+             <button
+              className="mt-2 w-full bg-orange-500 text-white py-2 rounded-lg hover:bg-orange-600 transition">
+               Buy Now
+             </button>
+          </div>
           </div>
         </div>
 

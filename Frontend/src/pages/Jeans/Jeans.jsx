105b42dc9bfdf130b2be0cfc8447d707c386eeb1
Gaurav Kumar <gauravkumar8414pq@gmail.com>
2026-08-03 21:55:58 +0530

    add Buy Now buttons in jeans.jsx

--- Frontend/src/pages/Jeans/Jeans.jsx
+++ Frontend/src/pages/Jeans/Jeans.jsx
@@ -53,11 +53,18 @@ function Jeans () {
             <h3 className="font-semibold text-lg">Regular Fit Jeans</h3>
             <p className="text-gray-500 text-sm">Comfort everyday wear</p>
             <p className="text-indigo-600 font-bold text-xl mt-2">₹999</p>
+
+          <div className="mt-4 flex gap-3">
             <button 
               onClick={() => addToCart({ id: 2, name: "Regular Fit Jeans", price: 999, img: Jeansimg2 })}
               className="mt-3 w-full bg-indigo-500 text-white py-2 rounded-lg hover:bg-indigo-600 transition">
               Add to Cart
             </button>
+             <button
+              className="mt-2 w-full bg-orange-500 text-white py-2 rounded-lg hover:bg-orange-600 transition">
+               Buy Now
+             </button>
+          </div>
           </div>
         </div>
 

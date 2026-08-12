e8bc7a122f70331d8a91c8ef3acf4383a6450457
Gaurav Kumar <gauravkumar8414pq@gmail.com>
2026-08-12 22:58:57 +0530

    add Buy Now buttons in men.jsx

--- Frontend/src/pages/Men/Men.jsx
+++ Frontend/src/pages/Men/Men.jsx
@@ -54,11 +54,17 @@ function Men () {
             <p className="text-gray-500 text-sm">Stylish casual wear</p>
             <p className="text-green-600 font-bold text-xl mt-2">₹599</p>
 
+         <div className="mt-4 flex gap-3">
             <button 
               onClick={() => addToCart({ id: 2, name: "Shirt", price: 599, img: Shirt2 })}
               className="mt-3 w-full bg-green-500 text-white py-2 rounded-lg hover:bg-green-600 transition">
               Add to Cart
             </button>
+            <button
+              className="mt-2 w-full bg-orange-500 text-white py-2 rounded-lg hover:bg-orange-600 transition">
+               Buy Now
+            </button>
+         </div>
           </div>
         </div>
 

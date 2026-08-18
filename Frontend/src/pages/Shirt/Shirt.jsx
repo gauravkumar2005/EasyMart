08640d790c700ed414b94c6cbaab065f06d396d8
Gaurav Kumar <gauravkumar8414pq@gmail.com>
2026-08-18 20:19:06 +0530

    add Buy Now buttons in shirt.jsx

--- Frontend/src/pages/Shirt/Shirt.jsx
+++ Frontend/src/pages/Shirt/Shirt.jsx
@@ -153,11 +153,18 @@ function Shirt () {
             <h3 className="font-semibold text-lg">Stylish Shirt</h3>
             <p className="text-gray-500 text-sm">Light & breathable</p>
             <p className="text-blue-600 font-bold text-xl mt-2">₹1099</p>
-            <button 
+
+          <div className="mt-4 flex gap-3">
+             <button 
               onClick={() => addToCart({ id: 6, name: "Stylish Shirt", price: 1099, img: Stylishshirtgirl })}
               className="mt-3 w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition">
               Add to Cart
             </button>
+             <button
+              className="mt-2 w-full bg-orange-500 text-white py-2 rounded-lg hover:bg-orange-600 transition">
+               Buy Now
+             </button>
+          </div>
           </div>
         </div>
 
